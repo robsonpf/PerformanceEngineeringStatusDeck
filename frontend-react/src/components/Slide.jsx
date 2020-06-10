11df13a995201e635464b5242015adfa38d79f52
Justin Chau <chausicle@gmail.com>
2020-06-10 14:26:31 -0700

    Add Slide header

--- frontend-react/src/components/Slide.jsx
+++ frontend-react/src/components/Slide.jsx
@@ -1,21 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { connect, } from 'react-redux';
-import './Slide.css';
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
 
+import Integration from "./Integration";
+import productTech from "../images/PR&T.png";
+import "./Slide.css";
 
 const Slide = () => {
   useEffect(() => {
-    const body = document.querySelector('body');
-    body.style.background = 'white';
+    const body = document.querySelector("body");
+    body.style.background = "white";
   });
 
   return (
     <div>
-      Slide Component
-
-
+      <div className="slide-header">
+        <h1 style={{ color: "#e20074" }}>Performance Enginnering Status</h1>
+        <div className="horizontal-center" style={{ display: "flex" }}>
+          <div className="update">
+            <span>{`Update as of UPDATE DATE & TIME`}</span>
+          </div>
+          <img src={productTech} alt="Product & Technology" />
+        </div>
+      </div>
+      <Integration />
     </div>
   );
-}
+};
 
 export default connect(null, null)(Slide);
