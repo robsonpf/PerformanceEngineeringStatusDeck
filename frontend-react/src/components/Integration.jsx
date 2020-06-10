88d1e119b02477f9ea2d2e8d84826455661ff5a0
Justin Chau <chausicle@gmail.com>
2020-06-10 14:26:05 -0700

    Initial Integration component

---
+++ frontend-react/src/components/Integration.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect, } from 'react-redux';
+
+const Integration = (props) => {
+  return (
+    <div>
+      Integration component
+    </div>
+  );
+};
+
+export default connect(null, null)(Integration);
