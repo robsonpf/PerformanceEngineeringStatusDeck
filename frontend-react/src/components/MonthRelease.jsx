d9466ab6fe9153d9eb9f4858359677b8ada35fe1
Justin Chau <chausicle@gmail.com>
2020-06-12 15:21:33 -0700

    Change semantic ui react component to HTML elements with class of semantic

--- frontend-react/src/components/MonthRelease.jsx
+++ frontend-react/src/components/MonthRelease.jsx
@@ -1,28 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Dropdown, Icon, Input } from "semantic-ui-react";
 
-
-
 const MonthRelease = (props) => {
-  const [ editSwitch, setEditSwitch ] = useState(false);
+  const [editSwitch, setEditSwitch] = useState(false);
 
-  const handleRenameMonth = () => {
-
-  }
-  console.log(editSwitch);
+  const handleRenameMonth = () => {};
+  
   return (
     <div id="month-release">
       <h3 className="headers">
         <div></div>
         <div>
-          { editSwitch ? <Input style={{color:"white"}} transparent placeholder="Month Name" /> : `MONTH Release` }
+          {editSwitch ? (
+            <div className="ui transparent input">
+              <input 
+                style={{ color: 'white', textAlign: 'center' }} 
+                placeholder="Month Name" 
+                type="text" />
+            </div>
+          ) : (
+            `MONTH Release`
+          )}
         </div>
         <div>
           <Dropdown icon="bars">
             <Dropdown.Menu>
-              <Dropdown.Item onClick={ () => setEditSwitch(!editSwitch) } icon="edit" text="Rename month"/>
-              <Dropdown.Item icon="plus" text="Add project"/>
-              <Dropdown.Item icon="trash" text="Delete slide"/>
+              <Dropdown.Item
+                onClick={() => setEditSwitch(!editSwitch)}
+                icon="edit"
+                text="Rename month"
+              />
+              <Dropdown.Item icon="plus" text="Add project" />
+              <Dropdown.Item icon="trash" text="Delete slide" />
             </Dropdown.Menu>
           </Dropdown>
         </div>
@@ -31,14 +40,10 @@ const MonthRelease = (props) => {
         <h4 id="key-callouts">Key Callouts:</h4>
         &#x25CF; P&T_PO_32 Boost XP I
       </div>
-      <h3 className="headers">
-        Risk & Mitigation
-      </h3>
-      <div id="risk-details">
-
-      </div>
+      <h3 className="headers">Risk & Mitigation</h3>
+      <div id="risk-details"></div>
     </div>
-  )
+  );
 };
 
 export default MonthRelease;
