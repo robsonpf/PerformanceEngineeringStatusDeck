17572436ba881ba965b0cc966acd0d31799d9d73
fariasrobson.rf@gmail.com <fariasrobson.rf@gmail.com>
2020-06-12 18:03:01 -0400

    Month change feature

--- frontend-react/src/components/MonthRelease.jsx
+++ frontend-react/src/components/MonthRelease.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import { Dropdown, Icon } from "semantic-ui-react";
+import React, { useState } from 'react';
+import { Dropdown, Icon, Input } from "semantic-ui-react";
+
+
 
 const MonthRelease = (props) => {
+  const [ editSwitch, setEditSwitch ] = useState(false);
+
+  const handleRenameMonth = () => {
+
+  }
+  console.log(editSwitch);
   return (
     <div id="month-release">
       <h3 className="headers">
+        <div></div>
         <div>
-
-        </div>
-        <div>
-          {`MONTH Release`}
+          { editSwitch ? <Input style={{color:"white"}} transparent placeholder="Month Name" /> : `MONTH Release` }
         </div>
         <div>
           <Dropdown icon="bars">
             <Dropdown.Menu>
-              <Dropdown.Item icon="edit" text="Rename month"/>
+              <Dropdown.Item onClick={ () => setEditSwitch(!editSwitch) } icon="edit" text="Rename month"/>
               <Dropdown.Item icon="plus" text="Add project"/>
               <Dropdown.Item icon="trash" text="Delete slide"/>
             </Dropdown.Menu>
